3f6de855f0e98e0aaf1a5a25215f9b1cc220ec0d
immortalkj <jain.kunal14195@gmail.com>
2016-01-23 20:38:54 +0530

    added pop up functionality and its interface

--- app/src/main/java/com/example/rishab/teamregistrations/SingletonNetworkClass.java
+++ app/src/main/java/com/example/rishab/teamregistrations/SingletonNetworkClass.java
@@ -1,7 +1,43 @@
 package com.example.rishab.teamregistrations;
 
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
 /**
- * Created by Rishab on 23-01-2016.
+ * Created by Rishab on 17-01-2016.
+ * This file creates a global request queue that has life same as that of app
  */
+
 public class SingletonNetworkClass {
+    private static SingletonNetworkClass single_netwrk;
+    private RequestQueue requestQueue;
+    private static Context mcontext;
+    //setting the context
+    public SingletonNetworkClass(Context context){
+        mcontext = context;
+        requestQueue=getRequestQueue();
+    }
+
+    public static synchronized SingletonNetworkClass getInstance(Context context){
+        if (single_netwrk==null){
+            single_netwrk=new SingletonNetworkClass(context);
+        }
+        return single_netwrk;
+    }
+    //get the request queue
+    public RequestQueue getRequestQueue(){
+        if (requestQueue==null){
+            requestQueue= Volley.newRequestQueue(mcontext.getApplicationContext());
+        }
+        return requestQueue;
+    }
+    //add a generic request to the the queue
+    public <T> void addToRequestQueue(Request<T> req){
+        getRequestQueue().add(req);
+    }
+
 }
+
